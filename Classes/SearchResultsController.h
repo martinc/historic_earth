eacab24d3e400d0aa148d9dfccf749a88fd5ddb7
martin <martin@ceperley.com>
2009-08-03 02:02:24 -0700

    loading search result images dynamically with three20

--- Classes/SearchResultsController.h
+++ Classes/SearchResultsController.h
@@ -9,10 +9,17 @@
 #import <UIKit/UIKit.h>
 #import "CustomButton.h"
 #import "MapViewController.h"
+#import "Three20/Three20.h"
 
-@interface SearchResultsController : UITableViewController {
+@interface SearchResultsController : UITableViewController <TTURLRequestDelegate> {
 	MapViewController *mapController;
 	NSMutableArray *listOfItems;
+	NSMutableArray *listOfImages;
+	UIActivityIndicatorView *loadingSpinner;  
+	
+	NSMutableData *receivedData;
+	
+	BOOL loadingResults;
 }
 
 - (UIImage *)scaleImage:(UIImage *) image maxWidth:(float) maxWidth maxHeight:(float) maxHeight;
--- Classes/SearchResultsController.m
+++ Classes/SearchResultsController.m
@@ -7,6 +7,9 @@
 //
 
 #import "SearchResultsController.h"
+#import "JSON.h"
+#import "Three20/Three20.h"
+#import "UIKit/UITableView.h"
 
 @implementation SearchResultsController
 
@@ -36,12 +39,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 	
+	
 	self.title = @"Results";
 	
 	self.tableView.backgroundColor = [UIColor clearColor];
 	
 	//self.tableView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.4];
+	
+	loadingSpinner = [[UIActivityIndicatorView alloc]
+					  initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
 
+	loadingSpinner.hidesWhenStopped = YES;
+	loadingSpinner.center = CGPointMake(320/2, 480/2);
+	//loadingSpinner.transform = CGAffineTransformMakeScale(4.0, 4.0);
+
+	[loadingSpinner startAnimating];
+	
+	[self.view addSubview:loadingSpinner];
 	
 	mapController = [[MapViewController alloc] initWithNibName:nil bundle:nil];
 
@@ -51,13 +65,16 @@
 	//Initialize the array.
 	listOfItems = [[NSMutableArray alloc] init];
 	
+	listOfImages = [[NSMutableArray alloc] init];
+	
 	//Add items
+	/*
 	[listOfItems addObject:@"1864"];
 	[listOfItems addObject:@"1926"];
 	[listOfItems addObject:@"1766"];
 	[listOfItems addObject:@"1655"];
 	[listOfItems addObject:@"1542"];
-	
+	*/
 	
 	/*
 	float tableWidth = 310.0;
@@ -87,6 +104,129 @@
 	self.tableView.tableFooterView = thebutton;
 	
 	*/	
+	
+	
+	
+	// create the request
+	NSURLRequest *theRequest=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://oldmapapp.com/test/sample.json"]
+											  cachePolicy:NSURLRequestUseProtocolCachePolicy
+										  timeoutInterval:15.0];
+	// create the connection with the request
+	// and start loading the data
+	NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+	if (theConnection) {
+		// Create the NSMutableData that will hold
+		// the received data
+		// receivedData is declared as a method instance elsewhere
+		receivedData=[[NSMutableData data] retain];
+		
+		loadingResults = YES;
+
+	} else {
+		// inform the user that the download could not be made
+	}
+	
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    // this method is called when the server has determined that it
+    // has enough information to create the NSURLResponse
+	
+    // it can be called multiple times, for example in the case of a
+    // redirect, so each time we reset the data.
+    // receivedData is declared as a method instance elsewhere
+    [receivedData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    // append the new data to the receivedData
+    // receivedData is declared as a method instance elsewhere
+    [receivedData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection
+  didFailWithError:(NSError *)error
+{
+    // release the connection, and the data object
+    [connection release];
+    // receivedData is declared as a method instance elsewhere
+    [receivedData release];
+	
+    // inform the user
+    NSLog(@"Connection failed! Error - %@ %@",
+          [error localizedDescription],
+          [[error userInfo] objectForKey:NSErrorFailingURLStringKey]);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // do something with the data
+    // receivedData is declared as a method instance elsewhere
+    NSLog(@"Succeeded! Received %d bytes of data",[receivedData length]);
+	
+	NSString* dataString =  [[NSString alloc]
+						  initWithData: receivedData
+						  encoding: NSUTF8StringEncoding];
+	
+	NSArray* jsonData = [dataString JSONValue];
+	
+	//NSLog(@"json object is : %@",jsonData);
+	
+	for(NSDictionary* mapData in jsonData){
+	
+		NSString* thename = [mapData objectForKey:@"name"];
+		[listOfItems addObject:thename];
+		[listOfImages addObject:[NSNull null]];
+		
+		NSString* theimage = [mapData objectForKey:@"image"];
+		
+//		NSLog(theimage);
+		
+		TTURLRequest* request = [TTURLRequest requestWithURL:theimage delegate:self];
+		
+		request.userInfo = [[NSNumber numberWithInt:[listOfItems count]-1] autorelease];
+		request.response = [[[TTURLImageResponse alloc] init] autorelease];
+
+		
+		[[TTURLRequestQueue mainQueue]  sendRequest: request];
+
+		
+	}
+	
+	loadingResults = NO;
+	[loadingSpinner stopAnimating];
+	
+	[self.tableView reloadData];	
+	
+    // release the connection, and the data object
+    [connection release];
+    [receivedData release];
+}
+
+- (void)requestDidFinishLoad:(TTURLRequest*)request
+{
+	
+	int rowloaded = ((NSNumber*)request.userInfo).intValue;
+	NSLog(@"successfully loaded row %d", rowloaded);
+	UIImage* imageLoaded = ((TTURLImageResponse *)request.response).image;
+	
+	
+	[listOfImages insertObject:imageLoaded atIndex: rowloaded];
+	
+	
+	NSIndexPath  *indx =  [NSIndexPath indexPathForRow:rowloaded inSection:0];
+
+	/*
+	[self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject: indx]
+						  withRowAnimation:UITableViewRowAnimationFade];
+	*/
+	[self.tableView reloadData];
+
+	
+	
+	
 }
 
 
@@ -126,7 +266,13 @@
 
 // Customize the number of rows in the table view.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [listOfItems count];
+	
+	if(loadingResults)
+		return 0;
+	else {
+		return [listOfItems count];
+	}
+
 }
 
 
@@ -141,9 +287,21 @@
     }
     
 	// Configure the cell.
+	/*
 	cell.imageView.image = [self scaleImage:[UIImage imageNamed:[NSString stringWithFormat:@"%d.png", indexPath.row+1]]
 								   maxWidth: 100
 								  maxHeight: 120];
+	*/
+	
+	if( indexPath.row < [listOfImages count] &&
+	   [listOfImages objectAtIndex:indexPath.row] != [NSNull null])
+	{
+	
+		cell.imageView.image = [self scaleImage:  (UIImage *)[listOfImages objectAtIndex:indexPath.row]
+									   maxWidth: 100
+									  maxHeight: 120];
+		
+	}
 	
 	
 	NSString *cellValue = [listOfItems objectAtIndex:indexPath.row];
