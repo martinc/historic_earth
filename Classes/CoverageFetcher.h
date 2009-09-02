88394a695e1cc7c7d6c7d94671f1a6eeb84f59a2
martin <martin@ceperley.com>
2009-09-02 00:24:43 -0400

    adding files

---
+++ Classes/CoverageFetcher.h
@@ -0,0 +1,16 @@
+//
+//  CoverageFetcher.h
+//  History
+//
+//  Created by Martin Ceperley on 9/1/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CoverageFetcher : NSObject {
+
+}
+
+@end
---
+++ Classes/CoverageFetcher.m
@@ -0,0 +1,14 @@
+//
+//  CoverageFetcher.m
+//  History
+//
+//  Created by Martin Ceperley on 9/1/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoverageFetcher.h"
+
+
+@implementation CoverageFetcher
+
+@end
